be3c170922be569bafc445059c0734fbbd9b4c6c
Johnny Lai <johnny.lai@me.com>
2016-01-08 14:15:02 -0800

    Don't bother to link the testdb docker image for utest if TESTDB_DOCKER_LABEL is empty

--- make/utest.mk
+++ make/utest.mk
@@ -3,6 +3,22 @@ utest: deps
 
 distutest: distutest.env distutest.run
 
+ifeq ($(TESTDB_DOCKER_LABEL),)
+#- No Test Database ------------------------------------------------------------
+distutest.env:
+
+distutest.run:
+	$(DOCKER) run --rm \
+	           -v $(SRCROOT):$(SRCROOT_D) \
+	           -v $(APP_SECRETS_ROOT):/etc/secrets \
+	           -w $(SRCROOT_D) \
+	           -e DEV_UID=$(DOCKER_DEV_UID) \
+	           -e DEV_GID=$(DOCKER_DEV_GID) \
+	           -e TEST_CONFIG_YML=$(TEST_CONFIG_YML_D) \
+	           $(DOCKER_DEVIMAGE) \
+	           make utest
+else
+#- Has Test Database -----------------------------------------------------------
 distutest.env:
 	-$(DOCKER) rm -f $(APP_NAME)-testdb
 	$(DOCKER) run -d --name $(APP_NAME)-testdb $(APP_DOCKER_LABEL)-testdb
@@ -19,3 +35,5 @@ distutest.run:
 	           -e TEST_CONFIG_YML=$(TEST_CONFIG_YML_D) \
 	           $(DOCKER_DEVIMAGE) \
 	           make utest
+
+endif
