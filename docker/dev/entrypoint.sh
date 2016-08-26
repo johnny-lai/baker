d752090fec91eb8ce2af60db5fa54ca29a8aa529
Johnny Lai <johnny.lai@me.com>
2016-08-26 13:06:34 -0700

    Use chmod in entrypoint.sh

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -3,12 +3,14 @@
 USER=root
 
 if [ $(whoami) == 'root' ] && [ $DEV_UID ]; then
-	rm -rf /home/dev
-
 	groupadd --gid $DEV_GID dev
 	adduser --disabled-password --gecos '' --uid $DEV_UID --gid $DEV_GID dev
 	# adduser dev sudo
 	# echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+	
+	# Fix permissions of home directory
+	# Since we mount /home/dev/.kube/config, this may be necessary
+	chown -R dev.dev /home/dev
 
 	USER=dev
 fi
