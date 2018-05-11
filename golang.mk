32319131d0bc1146080c8e367d74a65a02672bd3
Johnny Lai <johnny.lai@me.com>
2018-05-11 11:29:32 -0700

    Copy platform-specific executable instead of linking because the link in docker does not work when you are out of docker

--- golang.mk
+++ golang.mk
@@ -27,7 +27,7 @@ APP_ALL_ARCHS = $(patsubst %,$(APP)_%,$(APP_GO_ARCHS))
 
 #- Build -----------------------------------------------------------------------
 $(APP): $(APP_ALL_ARCHS)
-	ln -sf $(APP)_$(APP_GO_HOST_ARCH) $(APP)
+	cp $(APP) $(APP)_$(APP_GO_HOST_ARCH)
 
 $(APP)_%: $(APP_GO_SOURCES) $(APP_GO_DEPS) $(WORKSPACE_ROOT)/$(APP_GO_GLIDE_CHECK)
 	GOOS=$(subst _, GOARCH=,$*) $(GO_ENV) go build $(GO_CFLAGS) \
